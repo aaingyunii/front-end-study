df40243a70bbf6122f7e3c8885266a01ef418675
aaingyunii <dlsrbs98@gmail.com>
2023-12-18 02:58:45 +0900

    simple data binding

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -3,21 +3,29 @@ import './App.css';
 
 function App() {
   let posts = '고기고기';
+
   function 함수(){
     return 100;
   }
+
   return (
     <div className="App">
-      {/* navbar 만들기 */}
+      {/* navbar 생성 */}
       <div className="black-nav" style={ { color : 'wheat', fontSize : '30px'}} >
         개발 blog
       </div>
 
-      {/* 데이터 바인딩 예시 */}
+      {/* 데이터 바인딩 예시
       <img src={ logo } />
       <h4>{ posts }</h4>
-      <h4>{ 함수() }</h4>
+      <h4>{ 함수() }</h4> */}
 
+      {/* 리스트 생성 */}
+      <div className="list">
+        <h3>{ posts } </h3>
+        <p>12월 18일 발행</p>
+        <hr/>
+      </div>
 
     </div>
   );
