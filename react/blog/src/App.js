f991a5e2d22288f23d633f91a8585d07af87f13c
aaingyunii <dlsrbs98@gmail.com>
2023-12-17 23:55:42 +0900

    make navbar & data binding example

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -2,22 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = '고기고기';
+  function 함수(){
+    return 100;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* navbar 만들기 */}
+      <div className="black-nav" style={ { color : 'wheat', fontSize : '30px'}} >
+        개발 blog
+      </div>
+
+      {/* 데이터 바인딩 예시 */}
+      <img src={ logo } />
+      <h4>{ posts }</h4>
+      <h4>{ 함수() }</h4>
+
+
     </div>
   );
 }
