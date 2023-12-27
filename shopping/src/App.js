7e37abead71a7a1fac708cc87a531d6e4bc8568d
aaingyunii <dlsrbs98@gmail.com>
2023-12-27 23:58:38 +0900

    exercising router

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeaderNav from './Components/HeaderNav';
 import Background from './Components/Background';
-import getProducts from './Service/fetch';
+import Main from './Components/Main';
+import Login from './Components/Login';
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
         <HeaderNav/>
+        <Routes>
+          <Route path='/' element={<Main/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
         <Background/>
+      </BrowserRouter>
     </div>
   );
 }
-
-console.log(getProducts);
 
 export default App;
